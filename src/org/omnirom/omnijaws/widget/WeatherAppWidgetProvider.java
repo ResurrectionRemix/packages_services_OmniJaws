aeac40442ac4a39004ab52b5555f7977ab6ff723
maxwen <max.weninger@gmail.com>
2017-05-01 16:26:03 +0200

    OmniJaws: dont show OmniJaws error that prominent
    instead just show a small * to indicate an error. In best case
    the update has been triggered on screen on anyway and will
    be updated soon
    
    Also fix widget update on locale change
    
    Change-Id: Ie4d9d2d87395590babbe2255bd41c206bd0bf59d

--- src/org/omnirom/omnijaws/widget/WeatherAppWidgetProvider.java
+++ src/org/omnirom/omnijaws/widget/WeatherAppWidgetProvider.java
@@ -105,7 +105,8 @@ public class WeatherAppWidgetProvider extends AppWidgetProvider {
         if (LOGGING) {
             Log.i(TAG, "onReceive: " + action);
         }
-        if (action.equals(WEATHER_UPDATE)) {
+        if (action.equals(WEATHER_UPDATE)
+                || Intent.ACTION_LOCALE_CHANGED.equals(action)) {
             updateAllWeather(context);
         }
         if (action.equals(REFRESH_BROADCAST)) {
@@ -357,10 +358,13 @@ public class WeatherAppWidgetProvider extends AppWidgetProvider {
         widget.setViewVisibility(R.id.background_shadow, backgroundShadow ? View.VISIBLE : View.GONE);
         widget.setViewVisibility(R.id.progress_container, View.GONE);
         widget.setViewVisibility(R.id.condition_line, View.VISIBLE);
-        widget.setViewVisibility(R.id.current_weather_data, View.GONE);
-        widget.setTextViewText(R.id.no_weather_notice, errorReason == EXTRA_ERROR_DISABLED ?
-                context.getResources().getString(R.string.omnijaws_service_disabled) :
-                context.getResources().getString(R.string.omnijaws_service_error));
+        if (errorReason == EXTRA_ERROR_DISABLED) {
+            widget.setViewVisibility(R.id.current_weather_data, View.GONE);
+            widget.setTextViewText(R.id.no_weather_notice, context.getResources().getString(R.string.omnijaws_service_disabled));
+        } else {
+            widget.setViewVisibility(R.id.current_weather_data, View.VISIBLE);
+            widget.setTextViewText(R.id.no_weather_notice, " * ");
+        }
         widget.setViewVisibility(R.id.no_weather_notice, View.VISIBLE);
         widget.setImageViewBitmap(R.id.refresh, shadow(context.getResources(),
                 context.getResources().getDrawable(R.drawable.ic_menu_refresh)).getBitmap());
