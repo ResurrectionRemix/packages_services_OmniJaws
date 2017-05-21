c4e406deda843672767294517442a045a559f64d
Varun Date <date.varun123@gmail.com>
2017-05-22 00:18:06 +0530

    OwmniJaws Use own API key
    Reference http://gerrit.dirtyunicorns.com/#/c/6497/
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
+++ src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
@@ -38,7 +38,7 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     private static final int FORECAST_DAYS = 5;
     private static final String SELECTION_LOCATION = "lat=%f&lon=%f";
     private static final String SELECTION_ID = "id=%s";
-    private static final String API_KEY = "9f65b19b7a6648346dda93c6973a682c";
+    private static final String API_KEY = "e7af76bc8cd56dee34760b8aa120691e";
 
     private static final String URL_LOCATION =
             "http://api.openweathermap.org/data/2.5/find?q=%s&mode=json&lang=%s&appid=%s";
