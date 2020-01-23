40ba8ff600e91c82d95ac8741785ebf9407f037c
maxwen <max.weninger@gmail.com>
2020-01-24 00:07:53 +0100

    OmniJaws: dont hardcode string for unknown city
    Change-Id: Id7945a5602b67f3bf0c74ec10e90681455f98a4d

--- src/org/omnirom/omnijaws/METNorwayProvider.java
+++ src/org/omnirom/omnijaws/METNorwayProvider.java
@@ -8,6 +8,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
 import android.util.Log;
+import android.text.TextUtils;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.params.CoreProtocolPNames;
@@ -116,9 +117,14 @@ public class METNorwayProvider extends AbstractWeatherProvider {
                 windSpeed *= 3.6;
             }
 
+            String city = getNameLocality(coordinates);
+            if (TextUtils.isEmpty(city)) {
+                city = mContext.getResources().getString(R.string.omnijaws_city_unkown);
+            }
+
             WeatherInfo w = new WeatherInfo(mContext,
                     /* id */ coordinates,
-                    /* cityId */ getNameLocality(coordinates),
+                    /* cityId */ city,
                     /* condition */ conditionSummary,
                     /* conditionCode */ arrayWeatherIconToCode[getPriorityCondition(conditionSummary)],
                     /* temperature */ convertTemperature(weather.getDouble("air_temperature"), metric),
@@ -361,7 +367,7 @@ public class METNorwayProvider extends AbstractWeatherProvider {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "Unknown";
+        return null;
     }
 
     private static float convertTemperature(double value, boolean metric) {
