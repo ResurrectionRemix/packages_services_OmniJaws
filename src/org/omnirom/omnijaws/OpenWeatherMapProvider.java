RevengeOS/android_packages_services_OmniJaws

OmniJaws: Use openweather api for sunset/sunrise and change weather icons accordingly
Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
@@ -52,6 +52,8 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     private List<String> mKeys = new ArrayList<String>();
     private boolean mHasAPIKey;
     private int mRequestNumber;
+    private int sunrise;
+    private int sunset;
 
     public OpenWeatherMapProvider(Context context) {
         super(context);
@@ -132,6 +134,8 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
             JSONObject weather = conditions.getJSONArray("weather").getJSONObject(0);
             JSONObject conditionData = conditions.getJSONObject("main");
             JSONObject windData = conditions.getJSONObject("wind");
+            sunrise = conditions.getJSONObject("sys").getInt("sunrise");
+            sunset = conditions.getJSONObject("sys").getInt("sunset");
             ArrayList<DayForecast> forecasts =
                     parseForecasts(new JSONObject(forecastResponse).getJSONArray("list"), metric);
             String localizedCityName = conditions.getString("name");
@@ -261,7 +265,8 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     }
 
     private int mapConditionIconToCode(String icon, int conditionId) {
-
+    long ut2 = System.currentTimeMillis() / 1000L;
+    if ((sunrise < ut2) && (sunset > ut2)){
         // First, use condition ID for specific cases
         switch (conditionId) {
             // Thunderstorms
@@ -351,7 +356,99 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
             case 904: return 36; // hot
             case 905: return 24; // windy
             case 906: return 17; // hail
-        }
+        } 
+    } else {
+                // First, use condition ID for specific cases
+                switch (conditionId) {
+                    // Thunderstorms
+                    case 202:   // thunderstorm with heavy rain
+                    case 232:   // thunderstorm with heavy drizzle
+                    case 211:   // thunderstorm
+                        return 4;
+                    case 212:   // heavy thunderstorm
+                        return 3;
+                    case 221:   // ragged thunderstorm
+                    case 231:   // thunderstorm with drizzle
+                    case 201:   // thunderstorm with rain
+                        return 47;
+                    case 230:   // thunderstorm with light drizzle
+                    case 200:   // thunderstorm with light rain
+                    case 210:   // light thunderstorm
+                        return 45;
+        
+                    // Drizzle
+                    case 300:    // light intensity drizzle
+                    case 301:    // drizzle
+                    case 302:    // heavy intensity drizzle
+                    case 310:    // light intensity drizzle rain
+                    case 311:    // drizzle rain
+                    case 312:    // heavy intensity drizzle rain
+                    case 313:    // shower rain and drizzle
+                    case 314:    // heavy shower rain and drizzle
+                    case 321:    // shower drizzle
+                        return 9;
+        
+                    // Rain
+                    case 500:    // light rain
+                    case 501:    // moderate rain
+                    case 520:    // light intensity shower rain
+                    case 521:    // shower rain
+                    case 531:    // ragged shower rain
+                        return 11;
+                    case 502:    // heavy intensity rain
+                    case 503:    // very heavy rain
+                    case 504:    // extreme rain
+                    case 522:    // heavy intensity shower rain
+                        return 12;
+                    case 511:    // freezing rain
+                        return 10;
+        
+                    // Snow
+                    case 600: case 620: return 14; // light snow
+                    case 601: case 621: return 16; // snow
+                    case 602: case 622: return 41; // heavy snow
+                    case 611: case 612: return 18; // sleet
+                    case 615: case 616: return 5;  // rain and snow
+        
+                    // Atmosphere
+                    case 741:    // fog
+                        return 20;
+                    case 711:    // smoke
+                    case 762:    // volcanic ash
+                        return 22;
+                    case 701:    // mist
+                    case 721:    // haze
+                        return 21;
+                    case 731:    // sand/dust whirls
+                    case 751:    // sand
+                    case 761:    // dust
+                        return 19;
+                    case 771:    // squalls
+                        return 23;
+                    case 781:    // tornado
+                        return 0;
+        
+                    // clouds
+                    case 800:     // clear sky
+                        return 31;
+                    case 801:     // few clouds
+                        return 33;
+                    case 802:     // scattered clouds
+                        return 27;
+                    case 803:     // broken clouds
+                    case 804:     // overcast clouds
+                        return 29;
+        
+                    // Extreme
+                    case 900: return 0;  // tornado
+                    case 901: return 1;  // tropical storm
+                    case 902: return 2;  // hurricane
+                    case 903: return 25; // cold
+                    case 904: return 36; // hot
+                    case 905: return 24; // windy
+                    case 906: return 17; // hail
+                }
+    } 
 
         return -1;
     }

ResurrectionRemix/packages_services_OmniJaws

Change-Id: I063c7d0483f33ae3bd8e67e41df69c5984932e67

src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
@@ -54,6 +54,8 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     private List<String> mKeys = new ArrayList<String>();
     private boolean mHasAPIKey;
     private int mRequestNumber;
+    private int mSunrise;
+    private int mSunset;
 
     public OpenWeatherMapProvider(Context context) {
         super(context);
@@ -135,6 +137,8 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
             JSONObject weather = conditions.getJSONArray("weather").getJSONObject(0);
             JSONObject conditionData = conditions.getJSONObject("main");
             JSONObject windData = conditions.getJSONObject("wind");
+            mSunrise = conditions.getJSONObject("sys").getInt("sunrise");
+            mSunset = conditions.getJSONObject("sys").getInt("sunset");
             ArrayList<DayForecast> forecasts =
                     parseForecasts(new JSONObject(forecastResponse).getJSONArray("list"), metric);
             String localizedCityName = conditions.getString("name");
@@ -264,98 +268,190 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     }
 
     private int mapConditionIconToCode(String icon, int conditionId) {
+        long ut2 = System.currentTimeMillis() / 1000L;
+        if ((mSunrise < ut2) && (mSunset > ut2)){
+            // First, use condition ID for specific cases
+            switch (conditionId) {
+                // Thunderstorms
+                case 202:   // thunderstorm with heavy rain
+                case 232:   // thunderstorm with heavy drizzle
+                case 211:   // thunderstorm
+                    return 4;
+                case 212:   // heavy thunderstorm
+                    return 3;
+                case 221:   // ragged thunderstorm
+                case 231:   // thunderstorm with drizzle
+                case 201:   // thunderstorm with rain
+                    return 38;
+                case 230:   // thunderstorm with light drizzle
+                case 200:   // thunderstorm with light rain
+                case 210:   // light thunderstorm
+                    return 37;
 
-        // First, use condition ID for specific cases
-        switch (conditionId) {
-            // Thunderstorms
-            case 202:   // thunderstorm with heavy rain
-            case 232:   // thunderstorm with heavy drizzle
-            case 211:   // thunderstorm
-                return 4;
-            case 212:   // heavy thunderstorm
-                return 3;
-            case 221:   // ragged thunderstorm
-            case 231:   // thunderstorm with drizzle
-            case 201:   // thunderstorm with rain
-                return 38;
-            case 230:   // thunderstorm with light drizzle
-            case 200:   // thunderstorm with light rain
-            case 210:   // light thunderstorm
-                return 37;
+                // Drizzle
+                case 300:    // light intensity drizzle
+                case 301:    // drizzle
+                case 302:    // heavy intensity drizzle
+                case 310:    // light intensity drizzle rain
+                case 311:    // drizzle rain
+                case 312:    // heavy intensity drizzle rain
+                case 313:    // shower rain and drizzle
+                case 314:    // heavy shower rain and drizzle
+                case 321:    // shower drizzle
+                    return 9;
 
-            // Drizzle
-            case 300:    // light intensity drizzle
-            case 301:    // drizzle
-            case 302:    // heavy intensity drizzle
-            case 310:    // light intensity drizzle rain
-            case 311:    // drizzle rain
-            case 312:    // heavy intensity drizzle rain
-            case 313:    // shower rain and drizzle
-            case 314:    // heavy shower rain and drizzle
-            case 321:    // shower drizzle
-                return 9;
+                // Rain
+                case 500:    // light rain
+                case 501:    // moderate rain
+                case 520:    // light intensity shower rain
+                case 521:    // shower rain
+                case 531:    // ragged shower rain
+                    return 11;
+                case 502:    // heavy intensity rain
+                case 503:    // very heavy rain
+                case 504:    // extreme rain
+                case 522:    // heavy intensity shower rain
+                    return 12;
+                case 511:    // freezing rain
+                    return 10;
 
-            // Rain
-            case 500:    // light rain
-            case 501:    // moderate rain
-            case 520:    // light intensity shower rain
-            case 521:    // shower rain
-            case 531:    // ragged shower rain
-                return 11;
-            case 502:    // heavy intensity rain
-            case 503:    // very heavy rain
-            case 504:    // extreme rain
-            case 522:    // heavy intensity shower rain
-                return 12;
-            case 511:    // freezing rain
-                return 10;
+                // Snow
+                case 600: case 620: return 14; // light snow
+                case 601: case 621: return 16; // snow
+                case 602: case 622: return 41; // heavy snow
+                case 611: case 612: return 18; // sleet
+                case 615: case 616: return 5;  // rain and snow
 
-            // Snow
-            case 600: case 620: return 14; // light snow
-            case 601: case 621: return 16; // snow
-            case 602: case 622: return 41; // heavy snow
-            case 611: case 612: return 18; // sleet
-            case 615: case 616: return 5;  // rain and snow
+                // Atmosphere
+                case 741:    // fog
+                    return 20;
+                case 711:    // smoke
+                case 762:    // volcanic ash
+                    return 22;
+                case 701:    // mist
+                case 721:    // haze
+                    return 21;
+                case 731:    // sand/dust whirls
+                case 751:    // sand
+                case 761:    // dust
+                    return 19;
+                case 771:    // squalls
+                    return 23;
+                case 781:    // tornado
+                    return 0;
 
-            // Atmosphere
-            case 741:    // fog
-                return 20;
-            case 711:    // smoke
-            case 762:    // volcanic ash
-                return 22;
-            case 701:    // mist
-            case 721:    // haze
-                return 21;
-            case 731:    // sand/dust whirls
-            case 751:    // sand
-            case 761:    // dust
-                return 19;
-            case 771:    // squalls
-                return 23;
-            case 781:    // tornado
-                return 0;
+                // clouds
+                case 800:     // clear sky
+                    return 32;
+                case 801:     // few clouds
+                    return 34;
+                case 802:     // scattered clouds
+                    return 28;
+                case 803:     // broken clouds
+                case 804:     // overcast clouds
+                    return 30;
 
-            // clouds
-            case 800:     // clear sky
-                return 32;
-            case 801:     // few clouds
-                return 34;
-            case 802:     // scattered clouds
-                return 28;
-            case 803:     // broken clouds
-            case 804:     // overcast clouds
-                return 30;
+                // Extreme
+                case 900: return 0;  // tornado
+                case 901: return 1;  // tropical storm
+                case 902: return 2;  // hurricane
+                case 903: return 25; // cold
+                case 904: return 36; // hot
+                case 905: return 24; // windy
+                case 906: return 17; // hail
+            }
+        } else {
+                    // First, use condition ID for specific cases
+                    switch (conditionId) {
+                        // Thunderstorms
+                        case 202:   // thunderstorm with heavy rain
+                        case 232:   // thunderstorm with heavy drizzle
+                        case 211:   // thunderstorm
+                            return 4;
+                        case 212:   // heavy thunderstorm
+                            return 3;
+                        case 221:   // ragged thunderstorm
+                        case 231:   // thunderstorm with drizzle
+                        case 201:   // thunderstorm with rain
+                            return 47;
+                        case 230:   // thunderstorm with light drizzle
+                        case 200:   // thunderstorm with light rain
+                        case 210:   // light thunderstorm
+                            return 45;
 
-            // Extreme
-            case 900: return 0;  // tornado
-            case 901: return 1;  // tropical storm
-            case 902: return 2;  // hurricane
-            case 903: return 25; // cold
-            case 904: return 36; // hot
-            case 905: return 24; // windy
-            case 906: return 17; // hail
+                        // Drizzle
+                        case 300:    // light intensity drizzle
+                        case 301:    // drizzle
+                        case 302:    // heavy intensity drizzle
+                        case 310:    // light intensity drizzle rain
+                        case 311:    // drizzle rain
+                        case 312:    // heavy intensity drizzle rain
+                        case 313:    // shower rain and drizzle
+                        case 314:    // heavy shower rain and drizzle
+                        case 321:    // shower drizzle
+                            return 9;
+
+                        // Rain
+                        case 500:    // light rain
+                        case 501:    // moderate rain
+                        case 520:    // light intensity shower rain
+                        case 521:    // shower rain
+                        case 531:    // ragged shower rain
+                            return 11;
+                        case 502:    // heavy intensity rain
+                        case 503:    // very heavy rain
+                        case 504:    // extreme rain
+                        case 522:    // heavy intensity shower rain
+                            return 12;
+                        case 511:    // freezing rain
+                            return 10;
+
+                        // Snow
+                        case 600: case 620: return 14; // light snow
+                        case 601: case 621: return 16; // snow
+                        case 602: case 622: return 41; // heavy snow
+                        case 611: case 612: return 18; // sleet
+                        case 615: case 616: return 5;  // rain and snow
+
+                        // Atmosphere
+                        case 741:    // fog
+                            return 20;
+                        case 711:    // smoke
+                        case 762:    // volcanic ash
+                            return 22;
+                        case 701:    // mist
+                        case 721:    // haze
+                            return 21;
+                        case 731:    // sand/dust whirls
+                        case 751:    // sand
+                        case 761:    // dust
+                            return 19;
+                        case 771:    // squalls
+                            return 23;
+                        case 781:    // tornado
+                            return 0;
+
+                        // clouds
+                        case 800:     // clear sky
+                            return 31;
+                        case 801:     // few clouds
+                            return 33;
+                        case 802:     // scattered clouds
+                            return 27;
+                        case 803:     // broken clouds
+                        case 804:     // overcast clouds
+                            return 29;
+
+                        // Extreme
+                        case 900: return 0;  // tornado
+                        case 901: return 1;  // tropical storm
+                        case 902: return 2;  // hurricane
+                        case 903: return 25; // cold
+                        case 904: return 36; // hot
+                        case 905: return 24; // windy
+                        case 906: return 17; // hail
+                    }
         }
-
         return -1;
     }
 